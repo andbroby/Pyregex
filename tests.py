6d4a39f4683ccb4f7649e3688b411d1decbaf69f
Andreas Broby <and.broby@gmail.com>
2015-02-13 22:16:19 +0100

    Added tests for exceptions

--- tests.py
+++ tests.py
@@ -1,11 +1,22 @@
 import unittest
 from util import re_to_postfix
+from parsing_exceptions import MismatchedParentheses, UnrecognizedToken
 
 class Re_to_postfix_test(unittest.TestCase):
     def test_concatenation(self):
-        self.assertEqual(re_to_postfix('ab.ab'), ['ab', 'ab', '.'])
-        self.assertEqual(re_to_postfix('(ab.bb).b'), ['ab', 'bb', '.', 'b', '.'])
-        self.assertEqual(re_to_postfix('bbb.((a.b).b)'), ['bbb', 'a', 'b', '.', 'b', '.', '.'])
+        self.assertEqual(re_to_postfix('ab.ab'), ['a', 'b', '.', 'a', 'b', '.', '.'])
+        self.assertEqual(re_to_postfix('a(bb)*a'), ['a', 'b', 'b', '.', '*', '.', 'a', '.'])
+        self.assertEqual(re_to_postfix('(a(b(c)))'), ['a', 'b', 'c', '.', '.'])
+        self.assertEqual(re_to_postfix('()'), [])
+
+    def test_exceptions(self):
+        with self.assertRaises(MismatchedParentheses):
+            re_to_postfix('(a(bc)))')
+            re_to_postfix('(')
+
+    def test_implicit_concatenation(self):
+        self.assertEqual(re_to_postfix('abc'), ['a', 'b', 'c', '.', '.'])
+        self.assertEqual(re_to_postfix('a(bb)+a'), ['a','b','b','.','+','.','a','.'])
 
 
 if __name__ == '__main__':
