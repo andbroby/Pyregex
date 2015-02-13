57833511b75ea17bc2137f4b000540037415ac7b
Andreas Broby <and.broby@gmail.com>
2015-02-13 02:21:05 +0100

    Write unit tests for concatenation in the shunting yard algo

--- tests.py
+++ tests.py
@@ -1,21 +1,11 @@
 import unittest
-from shuntingyard import shunting_yard
+from util import re_to_postfix
 
-class ShuntingYardTests(unittest.TestCase):
-    def test_subtraction_addition(self):
-        self.assertEqual(shunting_yard('4+3+2-4-2-3+4+2') ,['4', '3', '+', '2', '+', '4', '-', '2', '-', '3', '-', '4', '+', '2', '+'])
-        self.assertEqual(shunting_yard('4-2-1-2'), ['4', '2', '-', '1', '-', '2', '-'])
-        self.assertEqual(shunting_yard('423+23+11'), ['423', '23', '+', '11', '+'])
-
-    def test_multiplication_division(self):
-        self.assertEqual(shunting_yard('4*3*5/2*3/2*4*5/5'), ['4', '3', '*', '5', '*', '2', '/', '3', '*', '2', '/', '4', '*', '5', '*', '5', '/'])
-        self.assertEqual(shunting_yard('4*2*1*4*3/2/3'), ['4','2','*','1','*','4','*','3','*','2','/','3','/'])
-        self.assertEqual(shunting_yard('423*123*112415*1'), ['423', '123', '*', '112415', '*', '1', '*'])
-                                                        
-                                      
-    def test_powers(self):
-        self.assertEqual(shunting_yard('4^2^3^5^4^3'), ['4','2','3', '5', '4', '3', '^', '^', '^', '^', '^'])
-        self.assertEqual(shunting_yard('23^1213^523^13'), ['23', '1213', '523', '13', '^', '^', '^'])
+class Re_to_postfix_test(unittest.TestCase):
+    def test_concatenation(self):
+        self.assertEqual(re_to_postfix('ab.ab'), ['ab', 'ab', '.'])
+        self.assertEqual(re_to_postfix('(ab.bb).b'), ['ab', 'bb', '.', 'b', '.'])
+        self.assertEqual(re_to_postfix('bbb.((a.b).b)'), ['bbb', 'a', 'b', '.', 'b', '.', '.'])
 
 
 if __name__ == '__main__':
