882f7b9a6980bce1bddf585ff3330bfde7b6185f
Andreas Broby <and.broby@gmail.com>
2015-02-17 02:02:39 +0100

    Finished up the NFA construction and cleaned up a bit

--- classes.py
+++ classes.py
@@ -12,18 +12,11 @@ class State():
                 self.transitions[token] = [state]
         else:
             self.transitions[self.token].append(state)
-            
     def __str__(self):
         return str(self.transitions)
-    def __bool__(self):
-        return False
-
-    type = "normal"
 
 class FinalState(State):
-    def __bool__(self):
-        return True
-    type = "final"
+    pass
 
 class NFAFragment():
     def __init__(self, start, out):
@@ -36,10 +29,6 @@ class NFAFragment():
         
 
 class NFA():
-    saved_NFA_state = {'index': None,
-                       'state': None,
-                       'guess': None}
-
     saved_states_stack = []
     
     def __init__(self, fragment):
@@ -49,9 +38,7 @@ class NFA():
     def has_valid_move(self, c):
         for state in self.current_states:
             if c in state.transitions and state.transitions[c]:
-                print("Got a valid move for " + str(c) + " in " + str(state.transitions))
                 return True
-            print("No valid move for " + str(c) + " in " + str(state.transitions))
         return False
 
     def is_at_epsilon(self):
@@ -62,11 +49,7 @@ class NFA():
     
     def step(self, string, i = 0, epsilon_state=None, guess=None, saved_index=None):
         c = None
-        print("\nNEW FUNCTION CALL")
-        print("We have choices: " + str(self.current_states[0].transitions))
-        print("Current index: " + str(i))
-        raw_input()
-
+        
         if isinstance(self.current_states[0], FinalState):
             return
         elif not self.is_at_epsilon and i >= len(string):
@@ -78,21 +61,16 @@ class NFA():
             c = ''
         
         if self.is_at_epsilon():
-            print("At epsilon state: " + str(self.current_states[0].transitions))
-
             saved_state = {}
             saved_state['state'] = self.current_states[:]
             saved_state['index'] = i
             if c is not None and c in self.current_states[0].transitions:
-                print("We can consume a token and transition with a character, so that's what we're doing")
                 self.current_states = self.current_states[0].transitions[c]
-                raw_input()
                 return self.step(string, i+1)
             choices = len(self.current_states[0].transitions["#e"])
             guess = random.randint(0, choices - 1)
             saved_state['guess'] = guess
             next_state = self.current_states[0].transitions["#e"][guess]
-            print("We chose " + str(next_state.transitions))
             self.saved_states_stack.append(saved_state)
             self.current_states = [next_state]
             return self.step(string, i)
@@ -110,8 +88,6 @@ class NFA():
             saved_guess = saved_NFA_state["guess"]
             new_guess = (saved_guess + 1) % len(saved_state[0].transitions["#e"])
             next_state = saved_state[0].transitions["#e"][new_guess]
-            print("Reverting state to " + str(saved_state))
-            print("New state is " + str(next_state))
             self.current_states = [next_state]
             return self.step(string, saved_index)
         return
